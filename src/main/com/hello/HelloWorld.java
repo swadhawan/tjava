9e7d5a5f10115e3a0ec1831020f68d1777418e70
Saurabh Wadhawan <saurabh18e@gmail.com>
2017-02-12 18:01:51 +0530

    2

--- src/main/com/hello/HelloWorld.java
+++ src/main/com/hello/HelloWorld.java
@@ -3,7 +3,7 @@ package com.hello;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World 1");
+        System.out.println("Hello, World 2");
     }
 
 }
